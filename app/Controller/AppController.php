dbf66f6f29ef63c7d2a0b9adf29e78944627cce6
valentino_nguyen <valentino.nguyen.92@gmail.com>
2015-05-01 23:03:52 +0700

    fix change language

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -28,7 +28,8 @@ class AppController extends Controller {
             } else {
                 $this->language = Configure::read('defaultLanguage'); // No language session => get default language from Config file
             }
-        }        
+        }   
+            
         
         $this->setLang($this->language); // call protected method setLang with the lang shortcode
         $this->set('language',$this->language); // send $this->language value to the view
@@ -72,6 +73,7 @@ class AppController extends Controller {
 
     protected function setLang($lang) { // protected method used to set the language
         $this->Session->write('Config.language', $lang); // write our language to session
+        $this->setCookieLanguage($lang);
         Configure::write('Config.language', $lang); // tell CakePHP that we're using this language        
     }
 
